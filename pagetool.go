c30ecf2424feec273bb793d30df5d2d0654d8acb
Michael Raimondi <mike@mikeraimondi.com>
2014-12-06 21:22:58 -0500

    Create directory for year

--- pagetool.go
+++ pagetool.go
@@ -3,18 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	filename := time.Now().Format("2006_01_02" + ".md")
+	t := time.Now()
+
+	// Create directory if it doesn't exist
+	directory := strconv.Itoa(t.Year())
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+		os.Mkdir(directory, 0755)
+	}
+
+	// Test for presence of file
+	filename := directory + "/" + t.Format("2006_01_02"+".md")
 	if _, err := os.Stat(filename); err == nil {
 		fmt.Println("Error: file exists")
 		return
 	}
+
+	// Create file
 	file, err := os.Create(filename)
 	if err != nil {
 		fmt.Println("Error: " + err.Error())
 	}
 	defer file.Close()
+
+	// Output filename
+	fmt.Println(file.Name())
 }
