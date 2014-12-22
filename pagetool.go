f80ca754457f007237b76a7259f1407cdec59e0b
Michael Raimondi <mike@mikeraimondi.com>
2014-12-21 21:56:33 -0500

    Open file for editing and prompt to commit to VCS

--- pagetool.go
+++ pagetool.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -26,10 +29,45 @@ func main() {
 	// Create file
 	file, err := os.Create(filename)
 	if err != nil {
-		fmt.Println("Error: " + err.Error())
+		fmt.Printf("Error creating file: %v\n", err)
+		return
 	}
-	defer file.Close()
+	file.Close()
+	fmt.Printf("File created: %v\n", file.Name())
 
-	// Output filename
-	fmt.Println(file.Name())
+	// Open file for editing
+	editCmd := strings.Split(os.Getenv("EDITOR"), " ")
+	editCmd = append(editCmd, file.Name())
+	err = exec.Command(editCmd[0], editCmd[1:]...).Run()
+	if err != nil {
+		fmt.Printf("Error opening editor: %v\n", err)
+		return
+	}
+
+	// Prompt for commit
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Commit? (y/n)")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	if input == "y" {
+		// Commit the changes
+		err = exec.Command("git", "add", file.Name()).Run()
+		if err != nil {
+			fmt.Printf("Error adding file to version control: %v\n", err)
+			return
+		}
+		err = exec.Command("git", "commit", "-m", "Done").Run()
+		if err != nil {
+			fmt.Printf("Error committing file: %v\n", err)
+			return
+		}
+		fmt.Println("Committed")
+		return
+	} else if input == "n" {
+		fmt.Println("Exiting")
+		return
+	} else {
+		fmt.Println("Unrecognized input, exiting")
+		return
+	}
 }
